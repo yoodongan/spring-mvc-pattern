133bbacf554f27d95da78b9bd74bbb0e51e1ed09
yoodongan <dbehddks519@naver.com>
2022-12-26 20:33:14 +0900

    implement each controller - modify Controller -> ControllerV1

--- src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -5,6 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public interface Controller {
+public interface ControllerV1 {
     void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
